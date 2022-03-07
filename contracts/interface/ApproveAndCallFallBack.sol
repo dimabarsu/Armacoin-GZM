0cb1e335e0f0d204e4b56000c0590084a950b956
Armacoin <46590238+dimabarsu@users.noreply.github.com>
2022-03-07 23:20:31 +0300

    Create ApproveAndCallFallBack.sol

---
+++ contracts/interface/ApproveAndCallFallBack.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+interface ApproveAndCallFallBack {
+    function receiveApproval(address _from, uint256 _amount, bytes calldata _data) external;
+}
